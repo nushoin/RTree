511fc62bbc3598c229752274995dd5ecabdeac03
Yariv <yariv@yariv-desktop.(none)>
2011-08-31 23:16:24 +0300

    vanilla version as taken from http://www.superliminal.com/sources/sources.htm

---
+++ Test.cpp
@@ -0,0 +1,105 @@
+//
+// Test.cpp
+//
+// This is a direct port of the C version of the RTree test program.
+//
+
+#include <stdio.h>
+#include "RTree.h"
+
+struct Rect
+{
+  Rect()  {}
+
+  Rect(int a_minX, int a_minY, int a_maxX, int a_maxY)
+  {
+    min[0] = a_minX;
+    min[1] = a_minY;
+
+    max[0] = a_maxX;
+    max[1] = a_maxY;
+  }
+
+
+  int min[2];
+  int max[2];
+};
+
+struct Rect rects[] = 
+{
+  Rect(0, 0, 2, 2), // xmin, ymin, xmax, ymax (for 2 dimensional RTree)
+  Rect(5, 5, 7, 7),
+  Rect(8, 5, 9, 6),
+  Rect(7, 1, 9, 2),
+};
+
+int nrects = sizeof(rects) / sizeof(rects[0]);
+
+Rect search_rect(6, 4, 10, 6); // search will find above rects that this one overlaps
+
+
+bool MySearchCallback(int id, void* arg) 
+{
+  printf("Hit data rect %d\n", id);
+  return true; // keep going
+}
+
+
+void main()
+{
+  RTree<int, int, 2, float> tree;
+
+  int i, nhits;
+  printf("nrects = %d\n", nrects);
+
+  for(i=0; i<nrects; i++)
+  {
+    tree.Insert(rects[i].min, rects[i].max, i); // Note, all values including zero are fine in this version
+  }
+
+  nhits = tree.Search(search_rect.min, search_rect.max, MySearchCallback, NULL);
+
+  printf("Search resulted in %d hits\n", nhits);
+
+  // Iterator test 
+  int itIndex = 0;
+  RTree<int, int, 2, float>::Iterator it;
+  for( tree.GetFirst(it); 
+       !tree.IsNull(it);
+       tree.GetNext(it) )
+  {
+    int value = tree.GetAt(it);
+    
+    int boundsMin[2] = {0,0};
+    int boundsMax[2] = {0,0};
+    it.GetBounds(boundsMin, boundsMax);
+    printf("it[%d] %d = (%d,%d,%d,%d)\n", itIndex++, value, boundsMin[0], boundsMin[1], boundsMax[0], boundsMax[1]);
+  }
+  
+  // Iterator test, alternate syntax
+  itIndex = 0;
+  tree.GetFirst(it);
+  while( !it.IsNull() )
+  {
+    int value = *it;
+    ++it;
+    printf("it[%d] %d\n", itIndex++, value);
+  }
+
+  getchar(); // Wait for keypress on exit so we can read console output
+
+  // Output:
+  //
+  // nrects = 4
+  // Hit data rect 1
+  // Hit data rect 2
+  // Search resulted in 2 hits
+  // it[0] 0 = (0,0,2,2)
+  // it[1] 1 = (5,5,7,7)
+  // it[2] 2 = (8,5,9,6)
+  // it[3] 3 = (7,1,9,2)  
+  // it[0] 0
+  // it[1] 1
+  // it[2] 2
+  // it[3] 3
+}
