03f32abb9350f1dad691f2e74f1200679f3f2c56
AlexVonB <AlexVonB@users.noreply.github.com>
2024-03-31 23:57:15 +0200

    added first gtests and cmake

---
+++ tests/basics.cpp
@@ -0,0 +1,131 @@
+#include "../RTree.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest-matchers.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+using namespace ::testing;
+
+typedef int ValueType;
+
+struct Rect {
+  Rect() {}
+
+  Rect(int a_minX, int a_minY, int a_maxX, int a_maxY) {
+    min[0] = a_minX;
+    min[1] = a_minY;
+
+    max[0] = a_maxX;
+    max[1] = a_maxY;
+  }
+
+  bool operator==(const Rect &o) const {
+    return min[0] == o.min[0] && min[1] == o.min[1] && max[0] == o.max[0] &&
+           max[1] == o.max[1];
+  }
+
+  int min[2];
+  int max[2];
+};
+
+const std::vector<Rect> rects = {
+    Rect(0, 0, 2, 2), // xmin, ymin, xmax, ymax (for 2 dimensional RTree)
+    Rect(5, 5, 7, 7),
+    Rect(8, 5, 9, 6),
+    Rect(7, 1, 9, 2),
+};
+
+const std::vector<ValueType> values = {0, 2, 3, 5};
+
+int nrects = rects.size();
+
+Rect search_rect(6, 4, 10,
+                 6); // search will find above rects that this one overlaps
+
+TEST(BasicTests, BasicTests) {
+  typedef RTree<ValueType, int, 2, float> MyTree;
+  MyTree tree;
+
+  int i, nhits;
+
+  ASSERT_EQ(nrects, 4);
+
+  for (i = 0; i < nrects; i++) {
+    tree.Insert(
+        rects[i].min, rects[i].max,
+        values[i]); // Note, all values including zero are fine in this version
+  }
+
+  MockFunction<bool(ValueType)> mock_function;
+  ON_CALL(mock_function, Call).WillByDefault(Return(true));
+  EXPECT_CALL(mock_function, Call(_)).Times(2);
+
+  nhits = tree.Search(search_rect.min, search_rect.max,
+                      mock_function.AsStdFunction());
+
+  ASSERT_EQ(nhits, 2);
+
+  std::vector<Rect> collectedRects = std::vector<Rect>();
+
+  // Iterator test
+  int itIndex = 0;
+  MyTree::Iterator it;
+  for (tree.GetFirst(it); !tree.IsNull(it); tree.GetNext(it)) {
+    int value = tree.GetAt(it);
+
+    int boundsMin[2] = {0, 0};
+    int boundsMax[2] = {0, 0};
+    it.GetBounds(boundsMin, boundsMax);
+    collectedRects.push_back(
+        Rect(boundsMin[0], boundsMin[1], boundsMax[0], boundsMax[1]));
+  }
+
+  EXPECT_THAT(rects, UnorderedElementsAreArray(collectedRects));
+
+  std::vector<ValueType> collectedValues = std::vector<ValueType>();
+
+  // Iterator test, alternate syntax
+  itIndex = 0;
+  tree.GetFirst(it);
+  while (!it.IsNull()) {
+    int value = *it;
+    ++it;
+    collectedValues.push_back(value);
+  }
+
+  EXPECT_THAT(values, UnorderedElementsAreArray(collectedValues));
+
+  // test copy constructor
+  MyTree copy = tree;
+
+  collectedRects.clear();
+
+  // Iterator test
+  itIndex = 0;
+  for (copy.GetFirst(it); !copy.IsNull(it); copy.GetNext(it)) {
+    int value = copy.GetAt(it);
+
+    int boundsMin[2] = {0, 0};
+    int boundsMax[2] = {0, 0};
+    it.GetBounds(boundsMin, boundsMax);
+    collectedRects.push_back(
+        Rect(boundsMin[0], boundsMin[1], boundsMax[0], boundsMax[1]));
+  }
+
+  EXPECT_THAT(rects, UnorderedElementsAreArray(collectedRects));
+
+  collectedValues.clear();
+
+  // Iterator test, alternate syntax
+  itIndex = 0;
+  copy.GetFirst(it);
+  while (!it.IsNull()) {
+    int value = *it;
+    ++it;
+    collectedValues.push_back(value);
+  }
+
+  EXPECT_THAT(values, UnorderedElementsAreArray(collectedValues));
+}
